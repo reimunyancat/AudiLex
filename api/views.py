091b0e8f0cf3cfa90aa646f532512fd6775292e9
reimunyancat <lasco647@gmail.com>
2025-11-23 01:28:04 +0900

    django_setting

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(['POST'])
+def transcribe_view(request):
+    """
+    API endpoint to transcribe a YouTube video.
+    """
+    if request.method == 'POST':
+        video_url = request.data.get('video_url')
+        if not video_url:
+            return Response({'error': 'video_url is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # TODO
+        
+        response_data = {
+            'message': 'Transcription process started.',
+            'video_url': video_url,
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
+
--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "audilex.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "audilex.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
