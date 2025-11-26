e28b27cf10c54db603fb803bcb0fee9c56d758a9
sh-owo <sngho0718@gmail.com>
2025-11-26 12:56:26 +0900

    Vidieo model 추가

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Vidieos(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    download_status = models.BooleanField()
+    download_dir = models.TextField()
+    transcript_status = models.BooleanField()
+    transcript_dir = models.TextField()
+    ipa_status = models.BooleanField()
+    ipa_dir = models.TextField() 
+    translate_status = models.BooleanField()
+    translate_dir = models.TextField()
